8e1b935934327909fd4c7de5a873fd120f41471f
Ahmed Al Jafarawy <prog.al.jafarawy@gmail.com>
2025-09-09 02:41:38 +0300

    finshing delete  functionalty with hard refesh

--- src/components/Popup/confirmDelete.jsx
+++ src/components/Popup/confirmDelete.jsx
@@ -1,12 +1,15 @@
 import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../../firebase";
-
+import { useNavigate } from "react-router";
 
 export default function ConfirmDelete({ setShowDeletePopup, docId }) {
+  const navigate = useNavigate();
+
   async function deleteItem() {
     try {
       const docRef = doc(db, "list_data", docId);
       await deleteDoc(docRef);
+      navigate(0);
     } catch (error) {
       console.error("Error deleting document:", error);
     }
@@ -22,7 +25,10 @@ export default function ConfirmDelete({ setShowDeletePopup, docId }) {
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/50" style={{ zIndex: "10" }}>
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black/50"
+      style={{ zIndex: "10" }}
+    >
       <div className="bg-white rounded-2xl shadow-lg w-80 p-6">
         <h2 className="text-lg font-semibold mb-4 text-gray-800">
           Are you sure you want to delete?
